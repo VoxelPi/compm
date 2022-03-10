ab5264c7d259defffd8643b6ec2209b7976cf887
VoxelPi <verblockt@gmail.com>
2022-03-10 12:05:53 +0100

    Added solution to problem 1.3

---
+++ ue/ue_0/problem_3.py
@@ -0,0 +1,22 @@
+def replace_word(s_in, s_find, s_replace):
+    words = s_in.split()
+    s_out = ""
+    for word in words:
+        if word == s_find:
+            s_out += " " + s_replace
+        else:
+            s_out += " " + word
+    return s_out.strip()
+
+def f(s: str, capitalize = False):
+    words = s.split()
+    if capitalize:
+        out = []
+        for word in words:
+            out.append(word.capitalize())
+        return out
+    else:
+        return words
+
+print(replace_word("this test is a test", "test", "word"))
+print(f("this test is a test"))
