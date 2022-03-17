7dccc0b61f31f1efc090dcbc7d9d2b4acefe11bc
VoxelPi <verblockt@gmail.com>
2022-03-17 12:39:33 +0100

    Added solution to problem 2.2

---
+++ ue/ue_02/problem_2.py
@@ -0,0 +1,34 @@
+class Complex:
+    def __init__(self, real, imag):
+        self.real = real
+        self.imag = imag
+
+    def add(self, z2):
+        return Complex(self.real + z2.real, self.imag + z2.imag)
+
+    def multiply(self, z2):
+        return Complex(self.real * z2.real - self.imag * z2.imag, self.real * z2.imag + self.imag * z2.real)
+
+    def divide(self, z2):
+        return Complex((self.real * z2.real + self.imag * z2.imag) / (z2.real**2 + z2.imag**2), (self.imag * z2.real - self.real * z2.imag) / (z2.real**2 + z2.imag**2))
+
+A = Complex(5, 3)
+B = Complex(2, -1)
+
+a = A.real + A.imag*1j
+b = B.real + B.imag*1j
+
+print("Add:")
+C = A.add(B)
+print(f"A + B = ({C.real}+{C.imag}j)")
+print(f"a + b = {a + b}")
+
+print("Multiply:")
+C = A.multiply(B)
+print(f"A * B = ({C.real}+{C.imag}j)")
+print(f"a * b = {a * b}")
+
+print("Divide:")
+C = A.divide(B)
+print(f"A / B = ({C.real}+{C.imag}j)")
+print(f"a / b = {a / b}")
