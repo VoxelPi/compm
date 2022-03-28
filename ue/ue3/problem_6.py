88dfdf00ed5575905eaa7ee758eb7791153e89b8
VoxelPi <verblockt@gmail.com>
2022-03-28 15:43:17 +0200

    Improve solution of problem 3.6

--- ue/ue3/problem_6.py
+++ ue/ue3/problem_6.py
@@ -1,14 +1,19 @@
 import os
 
-# print(f"TEST: {os.getcwd()}")
+# print(f"TEST: {os.getcwd()}") # Print current working directory.
+# os.remove("./ovid.txt") # Delete file.
 
-with open("./ovid.txt", "x") as file:
+with open("./ovid.txt", "w") as file:
     file.write("In nova fert animus mutatas dicere formas corpora; Di, coeptis nam vos mutastis et illas\nadspirate meis primaque ab origine mundi ad mea perpetuum deducite tempora carmen!")
 
+print("\nWithout spaces:")
 with open("./ovid.txt", "r") as file:
     for line in file.readlines():
-        print(line.replace(" ", ""))
+        print(line.replace(" ", ""), end="")
+    print("")
 
+print("\nReplaced:")
 with open("./ovid.txt", "r") as file:
     for line in file.readlines():
-        print(line.replace("di", "good").replace("Di", "Good"))
+        print(line.replace("di", "good").replace("Di", "Good"), end="")
+    print("")
