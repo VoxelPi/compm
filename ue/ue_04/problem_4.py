620d452f04d421a3883b3be68bce9e9e021f7cc4
VoxelPi <verblockt@gmail.com>
2022-03-31 12:41:22 +0200

    Added solution to problem 4.5

---
+++ ue/ue_04/problem_4.py
@@ -0,0 +1,12 @@
+from functools import reduce
+
+
+def list_to_sentence(list):
+    return reduce(lambda sentence, word: f"{sentence} {word}", map(lambda word : "".join(word), list))
+
+print(list_to_sentence([['i'], ['l', 'i', 'k', 'e'], ['c', 'a', 'k', 'e']]))
+
+def harmonic_series(n):
+    return reduce(lambda x, y: x + y, map(lambda x: 1/x, range(1, n+1)))
+
+print(harmonic_series(100))
