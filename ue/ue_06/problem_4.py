088f42eb0df221e037777d93e62f76994f26f378
VoxelPi <verblockt@gmail.com>
2022-04-29 19:37:52 +0200

    Fix histogram

--- ue/ue_06/problem_4.py
+++ ue/ue_06/problem_4.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-n = 11
+n = 10
 x = np.triu(np.arange(1, n+1)*np.ones(n).reshape(n, 1)).flatten()
 x = x[x != 0] - 1
 
 plt.figure()
-plt.hist(x, n, density=0.1)
+plt.hist(x, np.arange(0, n+1), density=0.1)
 plt.show()
