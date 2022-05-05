dd0a65c06a53dcd32c491c8aa857689e72edb377
VoxelPi <verblockt@gmail.com>
2022-05-05 13:00:09 +0200

    Added solution to problem 7.4

---
+++ ue/ue_07/problem_04.py
@@ -0,0 +1,12 @@
+import numpy as np
+from numpy.polynomial.polynomial import Polynomial
+import matplotlib.pyplot as plt
+from scipy.integrate import cumulative_trapezoid
+
+x = np.linspace(0, 2*np.pi)
+
+plt.figure()
+plt.plot(x, -np.cos(x) + 1, label="$\int{sin(x)}$", linestyle="--", linewidth=2)
+plt.plot(x, cumulative_trapezoid(np.sin(x), x, initial=0), label="cumulative_trapezoidal")
+plt.legend()
+plt.show()
